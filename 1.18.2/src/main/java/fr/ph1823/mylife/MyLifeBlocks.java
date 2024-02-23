5ded291536de8ba122e8777972964e5ae5da454f
Pierre-Henri Dupouy <pierre-henri.dupouy@etu.u-bordeaux.fr>
2024-02-23 11:45:30 +0100

    reefactor

--- 1.18.2/src/main/java/fr/ph1823/mylife/MyLifeBlocks.java
+++ 1.18.2/src/main/java/fr/ph1823/mylife/MyLifeBlocks.java
@@ -5,7 +5,6 @@ import fr.ph1823.mylife.block.CannabisBlock;
 import fr.ph1823.mylife.block.HoublonBlock;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
-import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
